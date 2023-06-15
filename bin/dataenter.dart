7b59bbb7e70d32c48b46c9f2c64f426401443d29
ajithbabu08 <ajithpersonaluse@gmail.com>
2023-06-15 22:37:32 +0530

    Initial commit

---
+++ bin/dataenter.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  String name="Ajith";
+  int age=21;
+  String email="ajith@gmail.com";
+  int phone=9857678909;
+  double mark=7.50;
+
+int num1=6, num2=8;
+
+  print("My name is $name");
+  print("I am $age years old");
+  print("My email id is $email");
+  print("My phone no is $phone");
+  print("My cgpa is $mark");
+
+  print("sum=$num1+$num2");
+}
---
+++ bin/varanddynamic.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  var data=1000.9;
+  data=2020;
+
+  print("data=$data");
+
+  //data='hello';//this shows error since the type of data is double so string value cant be assigned to double
+
+
+  dynamic data1="hello";
+  data1=10;
+  data1=100.2;
+}
